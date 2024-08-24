f248dbb9c13e241c56264d5ff8ca96bf2c21d180
Ayomide Lawal <ayomidelawal700@gmail.com>
2024-08-24 15:03:11 +0100

    validate user account before transaction process

--- api/transfer.go
+++ api/transfer.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 
 	db "github.com/devphasex/cedar-bank-api/db/sqlc"
@@ -9,9 +10,10 @@ import (
 )
 
 type TransferRequest struct {
-	FromAccountID int64   `json:"from_account_id"`
-	ToAccountID   int64   `json:"to_account_id"`
-	Amount        float64 `json:"amount"`
+	FromAccountID int64   `json:"from_account_id,required"`
+	ToAccountID   int64   `json:"to_account_id,required"`
+	Amount        float64 `json:"amount,required"`
+	Currency      string  `json:"currency,required" binding:"oneof=USD EUR CAD"`
 }
 
 var ErrFailed = errors.New("failed")
@@ -28,6 +30,14 @@ func (s *Server) transferTx(ctx *gin.Context) {
 		FromAccountID: req.FromAccountID,
 		ToAccountID:   req.ToAccountID,
 		Amount:        req.Amount,
+	}
+
+	if !s.validateAccount(ctx, arg.FromAccountID, req.Currency) {
+		return
+	}
+
+	if !s.validateAccount(ctx, arg.ToAccountID, req.Currency) {
+		return
 	}
 
 	tx, err := s.store.TransferTx(ctx, arg)
@@ -48,4 +58,28 @@ func (s *Server) transferTx(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, sucessResponse(tx))
+}
+
+func (s *Server) validateAccount(ctx *gin.Context, accountID int64, currency string) bool {
+	account, err := s.store.GetAccountByID(ctx, accountID)
+
+	if err != nil {
+		if errors.Is(err, db.ErrAccountNotFound) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return false
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return false
+	}
+
+	if account.Currency != currency {
+		ctx.JSON(http.StatusNotFound, errorResponse(
+			fmt.Errorf("account [%d] currency mismatch: expected %v but got %v",
+				accountID, account.Currency, currency)),
+		)
+		return false
+	}
+
+	return true
 }
