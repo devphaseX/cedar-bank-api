5a10f6a7e29db7f336adc68684921ea0ae50a179
Ayomide Lawal <ayomidelawal700@gmail.com>
2024-08-26 11:47:04 +0100

    add test for incorrect password check

--- util/hash/argon_test.go
+++ util/hash/argon_test.go
@@ -20,3 +20,20 @@ func TestPassword(t *testing.T) {
 	err = argonHash.Compare(passwordHashByte, passwordSaltByte, []byte(password))
 	require.NoError(t, err)
 }
+
+func TestIncorrectPassword(t *testing.T) {
+	argonHash := DefaultArgonHash()
+	password := "verysecurepassword"
+
+	passwordHash, err := argonHash.GenerateHash([]byte(password), nil)
+
+	require.NoError(t, err)
+
+	passwordHashStr, passwordSaltStr := ArgonStringEncode(passwordHash)
+
+	passwordHashByte, passwordSaltByte := ArgonStringDecode(passwordHashStr, passwordSaltStr)
+
+	enteredPassword := "wrongpassword"
+	err = argonHash.Compare(passwordHashByte, passwordSaltByte, []byte(enteredPassword))
+	require.Error(t, err)
+}
